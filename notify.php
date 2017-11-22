e5df373c44d1128b5b2d1687fd78e40ff26887bc
dedemao <admin@dedemao.com>
2017-11-22 16:03:32 +0800

    异步回调通知

---
+++ notify.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * 原生支付（扫码支付）及公众号支付的异步回调通知
+ * 说明：需要在native.php或者jsapi.php中的填写回调地址。例如：http://www.xxx.com/wx/notify.php
+ * 付款成功后，微信服务器会将付款结果通知到该页面
+ */
+header('Content-type:text/html; Charset=utf-8');
+$mchid = 'xxxxx';          //微信支付商户号 PartnerID 通过微信支付商户资料审核后邮件发送
+$appid = 'xxxxx';  //公众号APPID 通过微信支付商户资料审核后邮件发送
+$apiKey = 'xxxxx';   //https://pay.weixin.qq.com 帐户设置-安全设置-API安全-API密钥-设置API密钥
+$wxPay = new WxpayService($mchid,$appid,$apiKey);
+$result = $wxPay->notify();
+if($result){
+    //完成你的逻辑
+    //例如连接数据库，获取付款金额$result['cash_fee']，获取订单号$result['out_trade_no']，修改数据库中的订单状态等;
+}else{
+    echo 'pay error';
+}
+class WxpayService
+{
+    protected $mchid;
+    protected $appid;
+    protected $apiKey;
+    public function __construct($mchid, $appid, $key)
+    {
+        $this->mchid = $mchid;
+        $this->appid = $appid;
+        $this->apiKey = $key;
+    }
+
+    public function notify()
+    {
+        $config = array(
+            'mch_id' => $this->mchid,
+            'appid' => $this->appid,
+            'key' => $this->apiKey,
+        );
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if ($postObj === false) {
+            die('parse xml error');
+        }
+        if ($postObj->return_code != 'SUCCESS') {
+            die($postObj->return_msg);
+        }
+        if ($postObj->result_code != 'SUCCESS') {
+            die($postObj->err_code);
+        }
+        $arr = (array)$postObj;
+        unset($arr['sign']);
+        if (self::getSign($arr, $config['key']) == $postObj->sign) {
+            echo '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
+            return $arr;
+        }
+    }
+
+    /**
+     * 获取签名
+     */
+    public static function getSign($params, $key)
+    {
+        ksort($params, SORT_STRING);
+        $unSignParaString = self::formatQueryParaMap($params, false);
+        $signStr = strtoupper(md5($unSignParaString . "&key=" . $key));
+        return $signStr;
+    }
+    protected static function formatQueryParaMap($paraMap, $urlEncode = false)
+    {
+        $buff = "";
+        ksort($paraMap);
+        foreach ($paraMap as $k => $v) {
+            if (null != $v && "null" != $v) {
+                if ($urlEncode) {
+                    $v = urlencode($v);
+                }
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $reqPar = '';
+        if (strlen($buff) > 0) {
+            $reqPar = substr($buff, 0, strlen($buff) - 1);
+        }
+        return $reqPar;
+    }
+}
