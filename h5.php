9befac085db44fd6cc1b734eb24f5cd338390ef2
dedemao <admin@dedemao.com>
2018-05-27 14:59:43 +0800

    H5支付

--- h5.php
+++ h5.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * H5支付
  *
@@ -10,18 +10,28 @@
  * 5.请在微信外打开订单，进行支付（原因：H5支付不能直接在微信客户端内调起）
  */
 header('Content-type:text/html; Charset=utf-8');
+/** 请填写以下配置信息 */
 $mchid = 'xxxxx';          //微信支付商户号 PartnerID 通过微信支付商户资料审核后邮件发送
 $appid = 'xxxxx';  //微信支付申请对应的公众号的APPID
 $appKey = 'xxxxx';   //微信支付申请对应的公众号的APP Key
-$apiKey = 'xxxxx';  //https://pay.weixin.qq.com 帐户设置-安全设置-API安全-API密钥-设置API密钥
-
-$wxPay = new WxpayService($mchid,$appid,$apiKey);
+$apiKey = 'xxxxx';   //https://pay.weixin.qq.com 帐户设置-安全设置-API安全-API密钥-设置API密钥
 $outTradeNo = uniqid();     //你自己的商品订单号
 $payAmount = 0.01;          //付款金额，单位:元
 $orderName = '支付测试';    //订单标题
 $notifyUrl = 'http://www.xxx.com/wx/notify.php';     //付款成功后的回调地址(不要有问号)
-$payTime = time();      //付款时间
-$mwebUrl= $wxPay->createJsBizPackage($payAmount,$outTradeNo,$orderName,$notifyUrl,$payTime);
+$wapUrl = 'www.xxx.com';   //WAP网站URL地址
+$wapName = 'H5支付';       //WAP 网站名
+/** 配置结束 */
+
+$wxPay = new WxpayService($mchid,$appid,$apiKey);
+$wxPay->setTotalFee($payAmount);
+$wxPay->setOutTradeNo($outTradeNo);
+$wxPay->setOrderName($orderName);
+$wxPay->setNotifyUrl($notifyUrl);
+$wxPay->setWapUrl($wapUrl);
+$wxPay->setWapName($wapName);
+
+$mwebUrl= $wxPay->createJsBizPackage($payAmount,$outTradeNo,$orderName,$notifyUrl);
 echo "<h1><a href='{$mwebUrl}'>点击跳转至支付页面</a></h1>";
 exit();
 class WxpayService
@@ -29,22 +39,50 @@ class WxpayService
     protected $mchid;
     protected $appid;
     protected $apiKey;
+    protected $totalFee;
+    protected $outTradeNo;
+    protected $orderName;
+    protected $notifyUrl;
+    protected $wapUrl;
+    protected $wapName;
+
     public function __construct($mchid, $appid, $key)
     {
         $this->mchid = $mchid;
         $this->appid = $appid;
         $this->apiKey = $key;
     }
+
+    public function setTotalFee($totalFee)
+    {
+        $this->totalFee = $totalFee;
+    }
+    public function setOutTradeNo($outTradeNo)
+    {
+        $this->outTradeNo = $outTradeNo;
+    }
+    public function setOrderName($orderName)
+    {
+        $this->orderName = $orderName;
+    }
+    public function setWapUrl($wapUrl)
+    {
+        $this->wapUrl = $wapUrl;
+    }
+    public function setWapName($wapName)
+    {
+        $this->wapName = $wapName;
+    }
+    public function setNotifyUrl($notifyUrl)
+    {
+        $this->notifyUrl = $notifyUrl;
+    }
+
     /**
      * 发起订单
-     * @param float $totalFee 收款总费用 单位元
-     * @param string $outTradeNo 唯一的订单号
-     * @param string $orderName 订单名称
-     * @param string $notifyUrl 支付结果通知url 不要有问号
-     * @param string $timestamp 订单发起时间
      * @return array
      */
-    public function createJsBizPackage($totalFee, $outTradeNo, $orderName, $notifyUrl, $timestamp)
+    public function createJsBizPackage()
     {
         $config = array(
             'mch_id' => $this->mchid,
@@ -54,20 +92,20 @@ class WxpayService
         $scene_info = array(
             'h5_info' =>array(
                 'type'=>'Wap',
-                'wap_url'=>'1cwj.cn',
-                'wap_name'=>'H5支付',
+                'wap_url'=>$this->wapUrl,
+                'wap_name'=>$this->wapName,
             )
         );
         $unified = array(
             'appid' => $config['appid'],
             'attach' => 'pay',             //商家数据包，原样返回，如果填写中文，请注意转换为utf-8
-            'body' => $orderName,
+            'body' => $this->orderName,
             'mch_id' => $config['mch_id'],
             'nonce_str' => self::createNonceStr(),
-            'notify_url' => $notifyUrl,
-            'out_trade_no' => $outTradeNo,
+            'notify_url' => $this->notifyUrl,
+            'out_trade_no' => $this->outTradeNo,
             'spbill_create_ip' => $_SERVER['REMOTE_ADDR'],
-            'total_fee' => intval($totalFee * 100),       //单位 转为分
+            'total_fee' => intval($this->totalFee * 100),       //单位 转为分
             'trade_type' => 'MWEB',
             'scene_info'=>json_encode($scene_info)
         );
