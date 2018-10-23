8a4ed5222f7ea65f10ff349f2287fe0e036c76d4
dedemao <admin@dedemao.com>
2018-10-23 21:03:36 +0800

    公众号支付
    支付成功后，中文提示

--- jsapi.php
+++ jsapi.php
@@ -32,7 +32,11 @@ $jsApiParameters = json_encode($jsApiParameters);
                     <?php echo $jsApiParameters; ?>,
                     function(res){
                         WeixinJSBridge.log(res.err_msg);
-                        alert(res.err_code+res.err_desc+res.err_msg);
+						if(res.err_msg=='get_brand_wcpay_request:ok'){
+							alert('支付成功！');
+						}else{
+							alert('支付失败：'+res.err_code+res.err_desc+res.err_msg);
+						}
                     }
                 );
             }
